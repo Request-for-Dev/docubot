9f23b4c0507ab15cb0f26980775a42244c1f2c61
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-07-23 21:24:56 -0500

    Lint Staged: Test

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,45 +1,47 @@
-import { SignedOut, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import DLToggle from '@/c/Global/DLToggle'
+import { SignedOut, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import DLToggle from '@/c/Global/DLToggle';
 import { dark } from '@clerk/themes';
 
 const Header = () => {
   return (
-    <header className='flex items-center justify-between bg-light-600 dark:bg-dark-800 px-5 py-3'>
+    <header className='flex items-center justify-between bg-light-600 px-5 py-3 dark:bg-dark-800'>
       <Link href='/' className='flex items-center space-x-4'>
-        <Image src='/logo.png' alt='logo' width={45} height={45}  />
-        <h1 className='text-4xl font-bold'>DocuBot!</h1>
+        <Image src='/logo.png' alt='logo' width={45} height={45} />
+        <h1 className='text-4xl font-bold'>DocuBot</h1>
       </Link>
-      <div className='flex space-x-4 items-center'>
+      <div className='flex items-center space-x-4'>
         {/* User Button / Login Functions  */}
         <UserButton
-          showName={true}         
+          showName={true}
           appearance={{
             baseTheme: dark,
             elements: {
-              avatarBox: 'h-10 w-10'
-            }
+              avatarBox: 'h-10 w-10',
+            },
           }}
         />
         <SignedOut>
-          <Link className='px-3 py-2 dark:bg-dark-400 bg-light-700 dark:text-light-400 text-dark-800 rounded-md neon-lime border border-accent' href='/sign-up'>  
-          <button>
-            Sign Up
-          </button>
+          <Link
+            className='rounded-md border border-accent bg-light-700 px-3 py-2 text-dark-800 neon-lime dark:bg-dark-400 dark:text-light-400'
+            href='/sign-up'
+          >
+            <button>Sign Up</button>
           </Link>
-          <Link className='px-3 py-2 dark:bg-dark-400 bg-light-700 dark:text-light-400 text-dark-800 rounded-md neon-violet border border-accent2' href='/sign-in'>  
-          <button>
-            Sign In
-          </button>
+          <Link
+            className='rounded-md border border-accent2 bg-light-700 px-3 py-2 text-dark-800 neon-violet dark:bg-dark-400 dark:text-light-400'
+            href='/sign-in'
+          >
+            <button>Sign In</button>
           </Link>
         </SignedOut>
-          {/* Theme Toggle  */}
-          <DLToggle />
+        {/* Theme Toggle  */}
+        <DLToggle />
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
