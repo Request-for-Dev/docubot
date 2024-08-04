1149407e8be92c75a7ad903e21b3706bcb5dff42
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-08-04 13:01:37 -0500

    Push to Netlifly

--- src/app/dashboard/upgrade/page.tsx
+++ src/app/dashboard/upgrade/page.tsx
@@ -3,6 +3,7 @@
 'use client';
 
 import { createCheckoutSession } from '@/actions/createCheckoutSession';
+// import { createCheckoutSession } from '@/actions/createCheckoutSession';
 import { createStripePortal } from '@/actions/createStripePortal';
 import { Button } from '@/components/ui/button';
 import useSubscription from '@/hooks/useSubscription';
