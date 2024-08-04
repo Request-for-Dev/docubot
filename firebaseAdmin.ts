0a0876a8d155ad8eba302d8cc9cb937d86decf64
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-08-04 13:50:00 -0500

    JSON.parse the servi e key

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -3,7 +3,7 @@ import { getStorage } from 'firebase-admin/storage';
 import { initializeApp, getApps, App, getApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceKey = process.env.FIREBASE_SERVICE_KEY as string;
+const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_KEY as string);
 
 if (!serviceKey) {
   throw new Error('Firebase service key is not defined');
